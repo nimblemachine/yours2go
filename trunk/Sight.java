5091c414e57e40e9dc49edad54ab92106c24d8ee
willemvanwilligen <willemvanwilligen@01ba5cfa-cf3e-0410-bf94-21978dc8b85e>
2007-11-14 15:05:16 +0000

    added Sight.java. Crawler now also stores 10 descriptions for each sight in each city

---
+++ trunk/Sight.java
@@ -0,0 +1,27 @@
+import java.net.URL;
+
+class Sight {
+	
+	String city;
+	String name;
+	URL url;
+	
+	String[] descriptions;
+	
+	
+	Sight(){
+	}
+	
+	Sight(String city, String name, URL url, String[] descriptions){
+	    this.city = city;
+	    this.name = name;
+	    this.url = url;
+	    this.descriptions = descriptions;
+	}
+	
+	
+	
+	
+    
+
+}
